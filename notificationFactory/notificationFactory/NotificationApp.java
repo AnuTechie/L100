54eb76667ba539310f664788d98af0b25e1ce2e3
anu.keerthi <anu.keerthi@ei.study>
2025-06-23 22:23:45 +0530

    Initial commit

---
+++ notificationFactory/notificationFactory/NotificationApp.java
@@ -0,0 +1,17 @@
+package notificationFactory;
+
+import notificationFactory.notifications.Notification;
+import notificationFactory.factory.NotificationFactory;
+
+public class NotificationApp {
+    public static void main(String[] args) {
+        Notification email = NotificationFactory.createNotification("EMAIL");
+        email.notifyUser("Your bank statement is available.");
+
+        Notification sms = NotificationFactory.createNotification("SMS");
+        sms.notifyUser("Your OTP is 749102.");
+
+        Notification push = NotificationFactory.createNotification("PUSH");
+        push.notifyUser("₹10,000 credited to your account.");
+    }
+}
